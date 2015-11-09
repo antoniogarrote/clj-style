a16e519fc0fee67257cc520e3155ffa16cf41854
Antonio Garrote <antoniogarrote@gmail.com>
2015-11-09 15:44:53 +0000

    Updated project file

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clj_style "0.1.0-SNAPSHOT"
+  :description "Front-end for different Clojure static analyzers"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :eval-in-leiningen true
+  :plugins [[lein-cljfmt "0.3.0"]
+            [jonase/kibit "0.1.2"]
+            [jonase/eastwood "0.2.1"]])
